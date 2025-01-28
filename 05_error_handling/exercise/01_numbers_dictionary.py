c25593f25a0f801d5e58cc3a35aa8b465dd98f01
Nikisuhoto <nikisuhoto@gmail.com>
2025-01-28 08:11:16 +0200

    2

--- 05_error_handling/exercise/01_numbers_dictionary.py
+++ 05_error_handling/exercise/01_numbers_dictionary.py
@@ -1,15 +1,34 @@
-ls = [1, 2, 3, 4, 5, 6, 7]
+numbers_dictionary = {}
 
-ind = input()
+line = input()
 
-try:
-    print(ls[int(ind)])
+while line != "Search":
+    number_as_string = line
+    try:
+        number = int(input())
+    except ValueError:
+        print("The variable number must be an integer")
+    else:
+        numbers_dictionary[number_as_string] = number
 
-except ValueError:
-    print("Not valid input!")
-except IndexError:
-    print("Not in range!")
-else:
-    print("Suxess!")
-finally:
-    print("Fineshed!")
+    line = input()
+
+line = input()
+
+while line != "Remove":
+    searched = line
+    print(numbers_dictionary[searched])
+    line = input()
+
+line = input()
+
+while line != "End":
+    searched = line
+    try:
+        del numbers_dictionary[searched]
+    except KeyError:
+        print("Number does not exist in dictionary")
+
+    line = input()
+
+print(numbers_dictionary)
