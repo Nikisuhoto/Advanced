7c235f1b426fb52c558b40f5eaeb7c525bba3d8a
Nikisuhoto <nikisuhoto@gmail.com>
2025-01-30 14:26:37 +0200

    30-01

--- Exam_prep/Matrix/03_collecting_coins.py
+++ Exam_prep/Matrix/03_collecting_coins.py
@@ -21,16 +21,40 @@ moves = {
 
 player_row = player[0][0]
 player_col = player[0][1]
+matrix[player_row][player_col] = "0"
 
 while True:
-    command = input()
+    command = input().strip()
+    if command not in ("up", "down", "left", "right"):
+        continue
+
     new_player_row, new_player_col = moves[command](player_row, player_col)
+
+    if new_player_row < 0:
+        new_player_row = n - 1
+    if new_player_row >= n:
+        new_player_row = 0
+    if new_player_col < 0:
+        new_player_col = n - 1
+    if new_player_col >= n:
+        new_player_col = 0
+
     player.append([new_player_row, new_player_col])
-    if new_player_row < 0 or new_player_row >= n or new_player_col < 0 or new_player_col >= n:
-        exit()
+    if matrix[new_player_row][new_player_col] == "X":
+        print(f"Game over! You've collected {int(coins_collected / 2)} coins.")
+        break
 
-for i in range(len(matrix)):
-    print(matrix[i])
+    else:
+        map(int, matrix[new_player_row][new_player_col])
+        coins_collected += int(matrix[new_player_row][new_player_col])
+        matrix[new_player_row][new_player_col] = "0"
 
-print(player)
-print(walls)
+    player_row, player_col = new_player_row, new_player_col
+
+    if coins_collected > 100:
+        print(f"You won! You've collected {coins_collected} coins.")
+        break
+
+print("Your path:")
+for loc in player:
+    print(loc)
